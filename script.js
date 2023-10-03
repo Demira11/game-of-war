f2a6f81354b1de45fdbd7baad0fa810a1109e4c4
Demira11 <117664846+Demira11@users.noreply.github.com>
2023-10-03 18:23:55 -0400

    added pseudo code

---
+++ script.js
@@ -0,0 +1,21 @@
+
+
+
+
+/*
+pseudo code: 
+1. Get a deck of 52 cards
+2. Shuffle the deck
+3. Split the cards between 2 players
+
+Rounds:
+
+1. if player 1 or player 2 has 0 cards, they lose otherwise each player draws 1 cards
+2. check to see if the cards have equal value. If player 1 or player two has less then 3 cards, they lose. Otherwise 
+each player sets aside 3 cards and they go back to step 1.
+3. The player with the higest value gets all the cards.The cards compiled are set aside.
+4. repeat from step 1 */
+
+
+
+
