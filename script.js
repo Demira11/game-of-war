f38997a4bf8fa76561342932668241f59a5c1978
Demira11 <117664846+Demira11@users.noreply.github.com>
2023-10-04 19:39:31 -0400

    practiced OOP cards

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-class Cards {
+class Card {
     constructor(suit, rank, score){
-        this.suit = suitthis
+        this.suit = suit
         this.rank = rank
         this.score = score
     }
@@ -8,4 +8,21 @@ class Cards {
 
 
 
+class Deck {
+    constructor() {
+        this.card = [];
+        this.createDeck();
+    }
+    createDeck() {
+        const suits = ["hearts", "diamonds", "spades", "clubs"];
+        const ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "queen", "king", "Ace"];
 
+        for (let i = 0; i < suits.length; i++){
+          
+        }  
+    }
+}
+
+
+const max = new Deck();
+console.log(max)
